ee66c15563be454d877e418cf72ef1ff16a9636e
Marko <marko.pance@magiceden.io>
2023-02-07 02:50:43 +0700

    still trying

--- node_modules/@metaplex-foundation/js/src/plugins/bundlrStorage/BundlrStorageDriver.ts
+++ node_modules/@metaplex-foundation/js/src/plugins/bundlrStorage/BundlrStorageDriver.ts
@@ -1,5 +1,4 @@
 import type { default as NodeBundlr, WebBundlr } from '@bundlr-network/client';
-import * as _BundlrPackage from '@bundlr-network/client';
 import BigNumber from 'bignumber.js';
 import {
   Connection,
@@ -56,8 +55,6 @@ function _removeDoubleDefault(pkg: any) {
   return pkg;
 }
 
-const BundlrPackage = _removeDoubleDefault(_BundlrPackage);
-
 export type BundlrOptions = {
   address?: string;
   timeout?: number;
@@ -78,6 +75,12 @@ export type BundlrWalletAdapter = {
   ) => Promise<TransactionSignature>;
 };
 
+/// Size of Bundlr transaction header
+const HEADER_SIZE = 2_000;
+
+/// Minimum file size for cost calculation
+const MINIMUM_SIZE = 80_000;
+
 export class BundlrStorageDriver implements StorageDriver {
   protected _metaplex: Metaplex;
   protected _bundlr: WebBundlr | NodeBundlr | null = null;
@@ -96,8 +99,16 @@ export class BundlrStorageDriver implements StorageDriver {
     const price = await bundlr.getPrice(bytes);
 
     return bigNumberToAmount(
-      price.multipliedBy(this._options.priceMultiplier ?? 1.5)
+      price.multipliedBy(this._options.priceMultiplier ?? 1.1)
     );
+  }
+
+  async getUploadPriceForFiles(files: MetaplexFile[]): Promise<Amount> {
+    const bytes: number = files.reduce((sum, file) => {
+      return sum + HEADER_SIZE + Math.max(MINIMUM_SIZE, file.buffer.byteLength);
+    }, 0);
+
+    return this.getUploadPrice(bytes);
   }
 
   async upload(file: MetaplexFile): Promise<string> {
@@ -204,7 +215,7 @@ export class BundlrStorageDriver implements StorageDriver {
       typeof window === 'undefined' || window.process?.hasOwnProperty('type');
     let bundlr;
     if (isNode && isKeypairSigner(identity))
-      bundlr = this.initNodeBundlr(address, currency, identity, options);
+      bundlr = await this.initNodeBundlr(address, currency, identity, options);
     else {
       let identitySigner: IdentitySigner;
       if (isIdentitySigner(identity)) identitySigner = identity;
@@ -225,26 +236,22 @@ export class BundlrStorageDriver implements StorageDriver {
       // Check for valid bundlr node.
       await bundlr.utils.getBundlerAddress(currency);
     } catch (error) {
-      throw new FailedToConnectToBundlrAddressError(address, {
-        cause: error as Error,
-      });
+      throw new FailedToConnectToBundlrAddressError(address, error as Error);
     }
 
     return bundlr;
   }
 
-  initNodeBundlr(
+  async initNodeBundlr(
     address: string,
     currency: string,
     keypair: KeypairSigner,
     options: any
-  ): NodeBundlr {
-    return new BundlrPackage.default(
-      address,
-      currency,
-      keypair.secretKey,
-      options
+  ): Promise<NodeBundlr> {
+    const bPackage = _removeDoubleDefault(
+      await import('@bundlr-network/client')
     );
+    return new bPackage.default(address, currency, keypair.secretKey, options);
   }
 
   async initWebBundlr(
@@ -273,18 +280,16 @@ export class BundlrStorageDriver implements StorageDriver {
       },
     };
 
-    const bundlr = new BundlrPackage.WebBundlr(
-      address,
-      currency,
-      wallet,
-      options
+    const bPackage = _removeDoubleDefault(
+      await import('@bundlr-network/client')
     );
+    const bundlr = new bPackage.WebBundlr(address, currency, wallet, options);
 
     try {
       // Try to initiate bundlr.
       await bundlr.ready();
     } catch (error) {
-      throw new FailedToInitializeBundlrError({ cause: error as Error });
+      throw new FailedToInitializeBundlrError(error as Error);
     }
 
     return bundlr;
